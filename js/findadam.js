2f83510584f7bc6f3a1396763d236335a91da2ab
“Tan <dinhthai0312@gmail.com>
2024-06-23 17:54:24 +0700

    more develo[ findadam

--- js/findadam.js
+++ js/findadam.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
-    $(".bat").animate({
-        height:"300px",
-        opacity:1
-    },5000)
-    $(".bat").animate({
-        opacity:0
-    },0)
+    $(document).ready(function () {
+        $.ajax({
+            type: "get",
+            url: "../data/findadam.json",
+            dataType: "json",
+            success: function (response) {
+                console.log(response);
+                for (let index = 0; index < response.length; index++) {
+                    const element = response[index];
+                    $(".question-list").prepend(`
+                        <div class="card-container">
+                            <div class="card">
+                                <p class="ques">${element.ques}</p>
+                                <p class="ans">${element.ans}</p>
+                            </div>
+                        </div>
+                    `);
+                }
+            },
+            error: function (xhr, status, error) {
+                console.error("Lỗi khi tải dữ liệu:", error);
+            }
+        });
+    });
+    
 });
